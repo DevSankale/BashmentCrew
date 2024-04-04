a9dc69f3f7d153749c93a7b421da69bace530866
DevSankale <sankaledev@gmail.com>
2024-04-04 14:29:41 +0300

    add to cart button and remove from cart buttons functionality added.

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,23 @@
-const addbtn = document.querySelectorAll('.js-add-button');
-addbtnaddEventListener
+const joinbtn = document.querySelector('.js-join-button');
+ 
+joinbtn.addEventListener('click',()=>{
+  document.innerText = 'Joined';
+  console.log('Joined')
+});
+
+const addbtn=document.querySelectorAll('.js-add-button');
+addbtn.forEach((addbtn)=>{
+addbtn.addEventListener('click',()=>{
+  console.log('added');
+})
+});
+
+
+const removebtn = document.querySelectorAll('.js-remove-button');
+ removebtn.forEach((removebtn)=>{
+removebtn.addEventListener('click',()=>{
+  console.log('removed');
+})
+ })
+
+ 
